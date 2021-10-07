c26e9e5cfd3840f95ff141a835b5309ccaa69589
bscott4 <brodyscott12+dev@gmail.com>
2021-10-07 13:30:19 -0700

    tee'd up 2captcha, sniped textInstructions 🎯

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import * as puppeteer from 'puppeteer';
+import * as dotenv from 'dotenv';
+// import { sendCaptchaRequest } from './captchas';
+
+dotenv.config();
+const { EMAIL, PASSWORD } = process.env;
 
 (async () => {
   try {
-    const browser = await puppeteer.launch({ devtools: true });
+    const browser = await puppeteer.launch({
+      devtools: true,
+      args: [
+        '--disable-web-security',
+        '--disable-features=IsolateOrigins,site-per-process',
+      ],
+    });
     const incognito = await browser.createIncognitoBrowserContext();
     const page = await incognito.newPage();
-
     await page.goto('https://www.instacart.ca/');
     await page.click('span.css-utfnc');
     await page.click('span.css-utfnc');
@@ -18,18 +28,54 @@ import * as puppeteer from 'puppeteer';
     for (const input of inputs) {
       if (input.match(/^email/g)) {
         await page.focus(`#${input}`);
-        await page.keyboard.type(process.env["EMAIL"]);
+        await page.keyboard.type(EMAIL);
       }
       if (input.match(/^password/g)) {
         await page.focus(`#${input}`);
-        await page.keyboard.type(process.env["PASSWORD"]);
+        await page.keyboard.type(PASSWORD);
       }
     }
 
     await page.click('button[type=submit]');
+    await page.waitForTimeout(2000);
+
+    const recaptchaFrames = await page
+      .mainFrame()
+      .childFrames()
+      .filter((f) => f.url().match(/.recaptcha\/api2\/bframe/g));
+
+    console.log(`# of google bframes: ${recaptchaFrames.length}`);
+
+    // TODO: refactor for retries
+    if (recaptchaFrames.length !== 1) {
+      await incognito.close();
+      return;
+    }
+
+    const recaptchaChallenge = recaptchaFrames[0];
+
+    const textInstructions = await recaptchaChallenge.$eval(
+      'div.rc-imageselect-desc-no-canonical',
+      (div) => {
+        const childNodes = div.childNodes;
+        const textArr = Array.from(childNodes, (child) => child.textContent);
+        return textArr.join(' ').replace(/\s+/g, ' ');
+      },
+    );
+
+    const encodedImage = '';
+    await resolveCaptcha(encodedImage, textInstructions);
 
     await incognito.close();
   } catch (err) {
     console.error(err);
   }
 })();
+
+const resolveCaptcha = async (encodedImage, textInstructions) => {
+  console.log(
+    `here be the image and instructions:${encodedImage} ${textInstructions}`,
+  );
+  // const response = await sendCaptchaRequest(encodedImage, textInstructions);
+  // TODO: parse response, click tiles as per response, submit solution
+};
